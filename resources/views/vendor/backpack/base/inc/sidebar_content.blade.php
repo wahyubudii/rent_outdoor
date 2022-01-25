84e379a52dedc3bec340e4d3216f09952e79ef03
wahyubudii <79882296+wahyubudii@users.noreply.github.com>
2022-01-25 16:51:27 +0700

    added testimonials table

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,3 +2,4 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('tag') }}'><i class='nav-icon la la-question'></i> Tags</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('item') }}'><i class='nav-icon la la-question'></i> Items</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('testimonial') }}'><i class='nav-icon la la-question'></i> Testimonials</a></li>
--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -18,4 +18,5 @@ Route::group([
 ], function () { // custom admin routes
     Route::crud('tag', 'TagCrudController');
     Route::crud('item', 'ItemCrudController');
+    Route::crud('testimonial', 'TestimonialCrudController');
 }); // this should be the absolute last line of this file
