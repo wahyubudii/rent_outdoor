f86a6a41f31021d8ee827ddd89f768e2f58e612b
wahyubudii <79882296+wahyubudii@users.noreply.github.com>
2022-01-25 17:53:15 +0700

    testimonial clear

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Item;
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index() {
         $items = Item::all();
-        return view('dashboard.index', compact('items'));
+        $testimonials = Testimonial::all();
+        return view('dashboard.index', compact('items', 'testimonials'));
     }
 }
